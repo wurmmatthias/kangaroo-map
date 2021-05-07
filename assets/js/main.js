cb12541941c1d373b9d67140801e0b845dcce2dc
Matthias Wurm <wurm.matthias@gmx.net>
2021-05-07 13:16:03 +0200

    fixed minZoom

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ var mymap = L.map('mapid').setView([50.87, 8.02], 5);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
+  minZoom: 4,
   id: 'mapbox/streets-v10',
   tileSize: 512,
   zoomOffset: -1,
